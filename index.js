a3543bec1a28eae9bf964836d5573218b8f3f06a
danielpmc <danielpd93@gmail.com>
2023-02-07 17:45:14 +0000

    Update V2

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const db = require("quick.db")
-const nodeData = new db.table("nodeData");
-const dockerData = new db.table("dockerData");
+const { QuickDB, MySQLDriver } = require("quick.db");
 const si = require('systeminformation');
 const os = require("os");
 const pretty = require('prettysize');
 const moment = require("moment");
-const speedTest = require('speedtest-net');
 const fs = require('fs');
 const config = require('./config.json')
 const exec = require('child_process').exec;
-const PORT = "999"
+const PORT = config.WebServer;
 
+//Connect Quick.DB to MySQL
+(async () => {
+    const mysqlDriver = new MySQLDriver({
+        host: config.MySQL.Host,
+        user: config.MySQL.User,
+        password: config.MySQL.Pass,
+        database: config.MySQL.DB,
+    });
 
-//Modules
-const checkEmpty = require('./Modules/checkEmpty');
+    await mysqlDriver.connect().then(msg => {
+        console.log("Connected")
+    })
 
 //Automatic 30second git pull.
 setInterval(() => {
@@ -33,25 +39,18 @@ setInterval(() => {
 }, 30000)
 
 //Issue speedtest on startup
-speedtest();
 fetchData();
-dockers();
-
-/*
-//Speedtest every 3hours, Then send that data to the panel to store.
-setInterval(async () => {
-    speedtest()
-}, 10800000);
-*/
 
 //Get data and store in the database
 setInterval(async () => {
     fetchData()
 }, 2000)
 
+/*
 setInterval(async () => {
     dockers();
 }, 60000)
+*/
 
 app.get("/states", (req, res) => {
     if (req.headers.password === config.password) {
@@ -65,29 +64,22 @@ app.get("/states", (req, res) => {
     }
 })
 
-app.get("/empty", (req, res) => {
-    if (req.headers.password === config.password) {
-        res.send(checkEmpty.run());
-    } else {
-        res.send('Invalid or no password provided.')
-    }
-})
-
 app.get("/", (req, res) => {
-    res.send('Not sure what you expected to find here. This script just sends data to the panel and its all password protected so you can leave now :)')
+    res.sendStatus(418)
 });
 
 app.get('/stats', async function (req, res) {
     if (req.headers.password === config.password) {
+        const db = new QuickDB({ driver: mysqlDriver });
         let data = {
-            info: nodeData.fetch("data")//,
+            info: await db.get(os.hostname())//,
             //speedtest: nodeData.fetch("data-speedtest"),
             //docker: await si.dockerAll(),
             //discord: nodeData.fetch('discord')
         }
         res.send(data)
     } else {
-        res.send('Invalid or no password provided.')
+        res.sendStatus(403)
     }
 })
 
@@ -107,7 +99,7 @@ app.get('/wings', function (req, res) {
             exec(`service wings stop`)
         }
     } else {
-        res.send('Invalid or no password provided.')
+        res.sendStatus(403)
     }
 })
 
@@ -115,19 +107,19 @@ server.listen(PORT, function () {
     console.log("Waiting for connections...");
 });
 
-
-
 //DATA COLLECTION
 async function fetchData() {
+    const db = new QuickDB({ driver: mysqlDriver });
+
     //Data using the systeminformation package.
     let memdata = await si.mem();
     let diskdata = await si.fsSize();
     let netdata = await si.networkStats();
     let osdata = await si.osInfo();
-    let bios = await si.bios();
     let docker = await si.dockerInfo();
     let cl = await si.currentLoad();
     let cpudata = await si.cpu();
+    let wings = await si.services('wings');
 
     //OS UPTIME
     let uptime = os.uptime();
@@ -140,7 +132,7 @@ async function fetchData() {
     let mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
     let sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
 
-    nodeData.set("data", {
+    await db.set(os.hostname(), {
         servername: os.hostname(),
         cpu: cpudata.manufacturer + " " + cpudata.brand,
         cpuload: cl.currentload.toFixed(2),
@@ -150,91 +142,24 @@ async function fetchData() {
         memtotal: pretty(memdata.total),
         memusedraw: memdata.active,
         memtotalraw: memdata.total,
-        swapused: pretty(memdata.swapused),
-        swaptotal: pretty(memdata.swaptotal),
-        swapusedraw: memdata.swapused,
-        swaptotalraw: memdata.swaptotal,
-        diskused: pretty(diskdata[1].used),
-        disktotal: pretty(diskdata[1].size),
-        diskusedraw: diskdata[1].used,
-        disktotalraw: diskdata[1].size,
+        diskused: pretty(diskdata[0].used + diskdata[1].used),
+        disktotal: pretty(diskdata[0].size + diskdata[1].size),
+        diskusedraw: diskdata[0].used +  + diskdata[1].used,
+        disktotalraw: diskdata[0].size + diskdata[1].size,
         netrx: pretty(netdata[0].rx_bytes),
         nettx: pretty(netdata[0].tx_bytes),
         osplatform: osdata.platform,
         oslogofile: osdata.logofile,
         osrelease: osdata.release,
         osuptime: dDisplay + hDisplay + mDisplay + sDisplay,
-        biosvendor: bios.vendor,
-        biosversion: bios.version,
-        biosdate: bios.releaseDate,
-        servermonitorversion: "CUSTOM",
         datatime: Date.now(),
         dockercontainers: docker.containers,
         dockercontainersrunning: docker.containersRunning,
         dockercontainerspaused: docker.containersPaused,
         dockercontainersstopped: docker.containersStopped,
+        wingstatus: wings[0].running,
+        wingsramusage: wings[0].pmem,
         updatetime: moment().format("YYYY-MM-DD HH:mm:ss")
     });
 }
-
-async function speedtest() {
-    var timestamp = `${moment().format("YYYY-MM-DD HH:mm:ss")}`;
-    const speed = await speedTest({ maxTime: 5000, server: 15423, acceptLicense: true, acceptGdpr: true })
-    speed.on('data', async (data) => {
-        nodeData.set('data-speedtest', {
-            speedname: os.hostname(),
-            ping: data.server.ping,
-            download: data.speeds.download,
-            upload: data.speeds.upload,
-            updatetime: timestamp
-        });
-    })
-}
-
-async function dockers() {
-    const dockerAll = await si.dockerAll();
-    dockerData.set('data', {
-        dockerAll: dockerAll
-    });
-}
-
-async function overlay2clear() {
-    const path = require('path');
-    const fs = require('fs');
-
-    //Clear up space used by docker tmp files in overlay2
-    const directoryol2 = '/var/lib/docker/overlay2';
-    fs.readdir(directoryol2, (err, files) => {
-        files.forEach(file => {
-            if (fs.lstatSync(path.resolve(directoryol2, file)).isDirectory()) {
-                if (fs.existsSync(directoryol2 + "/" + file + "/diff/tmp/")) {
-                    exec(`rm -rf ${directoryol2}/${file}/diff/tmp/\*`)
-                }
-            } else {
-                //Do nothing for files (if they are files in there. then uh oh...)
-            }
-        });
-    });
-
-    //Clear up space used by docker tmp folders in merged
-    const directorymerged = '/var/lib/docker/overlay2';
-    fs.readdir(directorymerged, (err, files) => {
-        files.forEach(file => {
-            if (fs.lstatSync(path.resolve(directorymerged, file)).isDirectory()) {
-                if (fs.existsSync(directorymerged + "/" + file + "/merged/tmp/")) {
-                    console.log(`${directorymerged}/${file}/merged/tmp/\*`)
-                    exec(`rm -rf ${directorymerged}/${file}/merged/tmp/\*`)
-                }
-            } else {
-                //Do nothing for files (if they are files in there. then uh oh...)
-            }
-        });
-    });
-}
-
-//Stop wings from giving "Too many open files" error due to docker
-exec(`ulimit -Hn 32768`)
-exec(`ulimit -Sn 32768`)
-exec(`sysctl fs.inotify.max_user_instances=5120  `)
-exec(`sysctl fs.inotify.max_user_watches=2621440  `)
-exec(`sysctl fs.inotify.max_queued_events=655360  `)
+})();
